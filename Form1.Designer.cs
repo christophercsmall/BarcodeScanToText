c519bb6b4cdac11547c38fd653805c5a7435b6c4
Christopher Small <christopher.c.small@gmail.com>
2017-07-24 01:04:15 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BarcodeScanToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.msg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scannedData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 603);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Scanner XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.Location = new System.Drawing.Point(33, 37);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(76, 13);
+            this.msg.TabIndex = 1;
+            this.msg.Text = "Message Here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Barcode Data Here";
+            // 
+            // scannedData
+            // 
+            this.scannedData.Location = new System.Drawing.Point(207, 82);
+            this.scannedData.Multiline = true;
+            this.scannedData.Name = "scannedData";
+            this.scannedData.Size = new System.Drawing.Size(604, 539);
+            this.scannedData.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 674);
+            this.Controls.Add(this.scannedData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox scannedData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreScanner;
+
+namespace BarcodeScanToText
+{
+    public partial class Form1 : Form
+    {
+        CCoreScannerClass cCoreScannerClass;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnBarcodeEvent(short eventType, ref string scanData)
+        {
+            string barcode = scanData;
+            this.Invoke((MethodInvoker)delegate { scannedData.Text = barcode; });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cCoreScannerClass = new CoreScanner.CCoreScannerClass();
+
+                short[] scannerTypes = new short[1];
+                scannerTypes[0] = 1;
+                short numberOfScannerTypes = 1;
+                int status;
+
+                cCoreScannerClass.Open(0, scannerTypes, numberOfScannerTypes, out status);
+
+                cCoreScannerClass.BarcodeEvent += new _ICoreScannerEvents_BarcodeEventEventHandler(OnBarcodeEvent);
+
+                short numberOfScanners;
+                int[] connectedScannerIDList = new int[10];
+                string outXML;
+
+                cCoreScannerClass.GetScanners(out numberOfScanners, connectedScannerIDList, out outXML, out status);
+
+                int opcode = 1001;
+                string inXML = "<inArgs>" +
+                                    "<cmdArgs>" +
+                                        "<arg-int>1</arg-int>" +
+                                        "<arg-int>1</arg-int>" +
+                                    "</cmdArgs>" +
+                                "</inArgs>";
+
+                cCoreScannerClass.ExecCommand(opcode, ref inXML, out outXML, out status);
+
+
+            }
+            catch (Exception exp)
+            {
+                msg.Text = exp.ToString();
+            }
+            //cCoreScannerClass = new CoreScanner.CCoreScannerClass();
+
+            //short numberOfScanner;
+            //int[] connectedScannerIDList = new int[10];
+            //string outXML;
+            //cCoreScannerClass.GetScanners(out numberOfScanner, connectedScannerIDList, out outXML, out status);
+            //msg.Text = outXML;
+
+
+            //if (status == 0)
+            //{
+            //    msg.Text = "CoreScanner API: Open Successful";
+            //    //Console.WriteLine("CoreScanner API: Open Successful");
+            //}
+            //else
+            //{
+            //    msg.Text = "CoreScanner API: Open Failed";
+            //    //Console.WriteLine("CoreScanner API: Open Failed");
+            //}
+
+
+
+        }
+
+    public void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
